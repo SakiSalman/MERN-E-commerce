86f487c0d76730bfcfcaf843e21653b69578bd0e
Saki Salman <95974163+SakiSalman@users.noreply.github.com>
2023-03-27 02:42:50 +0600

    user register routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,13 +1,13 @@
 import express from 'express'
-import { getUsers } from '../controllers/userControllers.js'
+import { registerUser } from '../controllers/userControllers.js'
 
 
 const router = express.Router()
 
 
 
-// get users -- admin
-router.get('/', getUsers)
+// REGISTER ROUTE
+router.post('/register', registerUser)
 
 
 //  export router
