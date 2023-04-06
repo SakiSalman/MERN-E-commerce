be43e1e742cf3b19ff00695285af163046307afb
Saki Salman <95974163+SakiSalman@users.noreply.github.com>
2023-04-07 03:55:59 +0600

    installed react router dom and setup routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,28 @@ import './App.css';
 import Layout from './Components/Layouts/Layout';
 import Home from './Pages/Home/Home';
 import 'boxicons/css/boxicons.min.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Route, Routes } from 'react-router-dom';
+import Shop from './Pages/Shop/Shop';
+import Blog from './Pages/Blog/Blog';
+import Contact from './Pages/Contact/Contact';
+import Faq from './Pages/FAQ/Faq';
+import Signup from './Pages/Auth/SignUp/Signup';
+import Signin from './Pages/Auth/SignIn/Signin';
 function App() {
   return (
     <div className="App">
-      <Home/>
+
+      <Routes>
+        <Route path='/' element={ <Home/>}/>
+        <Route path='/shop' element={ <Shop></Shop>}/>
+        <Route path='/blog' element={ <Blog></Blog>}/>
+        <Route path='/contact' element={ <Contact></Contact>}/>
+        <Route path='/faq' element={ <Faq></Faq>}/>
+        <Route path='/sign-up' element={ <Signup></Signup>}/>
+        <Route path='/sign-in' element={<Signin></Signin>}/>
+      </Routes>
+     
     </div>
   );
 }
